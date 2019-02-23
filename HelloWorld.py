f0e9d5a754fdad3b8fc6bf95e8204dbfd94301cc
Divakar Babu Budumuru <divakar.budumuru@netenrich.com>
2019-02-23 10:32:30 +0530

    hello world created

---
+++ HelloWorld.py
@@ -0,0 +1,10 @@
+print("Hello world!")
+
+name = input("Enter your name here: ")
+print("Welcome "+name)
+
+print('Now I am working on lists')
+print('creating a new list with name list')
+
+list = list([1, 2, 3, 4, 5])
+print(list)
